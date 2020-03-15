cc601f07d3a76543e806ea7850cc760234531c6d
GreatGonthar <60307580+GreatGonthar@users.noreply.github.com>
2020-03-15 12:28:11 +1000

    начало проекта

---
+++ main.py
@@ -0,0 +1,6 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5.QtCore import Qt, QPoint, QBasicTimer, QSize
+
+
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
